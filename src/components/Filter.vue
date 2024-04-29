6e2da36082a796791e1ca8324574f1fbfae1e545
iskeym <ggwpnah74@mail.ru>
2024-04-30 02:55:29 +0800

    fixed Filter

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="filter">
-      <input type="text" v-model="searchUniversityText" placeholder="Search university...">
+      <input type="text" v-model="searchUniversityText" @input="updateSearchText" placeholder="Search university...">
       <input type="text" v-model="searchText" placeholder="Search country...">
       <select @change="updateCountry" v-model="country">
+        <option value=""></option>
         <option v-for="country in filteredCountries" :value="country">{{ country }}</option>
       </select>
     </div>
   </template>
   
 <script>
+  import axios from 'axios';
+
   export default {
     data() {
       return {
@@ -23,12 +26,28 @@
         return this.countries.filter(country => country.toLowerCase().includes(this.searchText.toLowerCase()));
       },
     },
+    async mounted() {
+      try {
+        const response = await axios.get('http://universities.hipolabs.com/search?_limit=10');
+        this.universities = response.data;
+        this.countries = [...new Set(this.universities.map(university => university.country))];
+        console.log(this.countries);
+        this.loading = false;
+      } catch (error) {
+        console.error(error);
+        this.loading = false;
+      }
+    },
     methods: {
       async updateCountry(event) {
         this.country = event.target.value;
         this.$emit('update-country', this.country);
       },
+      async updateSearchText(event) {
+        this.searchUniversityText = event.target.value;
+        this.$emit('update-search-text', this.searchUniversityText);
+      }
     },
   };
-  </script>
+</script>
   
--- src/views/UniversityList.vue
+++ src/views/UniversityList.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <Filter @update-country="updateCountry"></Filter>
+    <Filter @update-country="updateCountry" @update-search-text="updateSearchText"></Filter>
     <h1 v-if="loading">Идёт загрузка...</h1>
     <ul v-else>
       <li v-for="(university, index) in paginatedUniversities" :key="index">
-        <a :href="university.web_pages[0]">{{ university.name }}</a>
+        <a :href="university.web_pages[0]">{{ university.name }}</a> - <p>{{ university.country }}</p>
         <HeartBtn :isFavorite="checkFavorite(university)" @add-to-favorites="addToFavorites(university)"/>
       </li>
       <div>
@@ -38,9 +38,7 @@ export default {
   },
   computed: {
     paginatedUniversities() {
-      const filteredUniversities = this.universities.filter(university => {
-        return university.name.toLowerCase().includes(this.searchUniversityText ? this.searchUniversityText.toLowerCase() : '');
-      });
+      const filteredUniversities = this.universities
       const startIndex = (this.currentPage - 1) * this.itemsPerPage;
       const endIndex = Math.min(startIndex + this.itemsPerPage, filteredUniversities.length);
       return filteredUniversities.slice(startIndex, endIndex);
@@ -63,7 +61,6 @@ export default {
       const response = await axios.get('http://universities.hipolabs.com/search?_limit=10');
       this.universities = response.data;
       this.countries = [...new Set(this.universities.map(university => university.country))];
-      console.log(this.countries);
       this.loading = false;
     } catch (error) {
       console.error(error);
@@ -71,6 +68,13 @@ export default {
     }
   },
   methods: {
+    updateSearchText(event) {
+      this.currentPage = 1;
+      this.searchUniversityText = event;
+      this.universities = this.universities.filter(university => {
+        return university.name.toLowerCase().includes(this.searchUniversityText ? this.searchUniversityText.toLowerCase() : '');
+      });
+    },
     checkFavorite(university) {
       let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
       const index = favorites.findIndex(item => item.name === university.name);
@@ -107,10 +111,9 @@ export default {
       }
     },
     async updateCountry(event) {
-      this.country = event.target.value;
       this.currentPage = 1;
       try {
-        const response = await axios.get(`http://universities.hipolabs.com/search?country=${this.country}&_page=1&_limit=10`);
+        const response = await axios.get(`http://universities.hipolabs.com/search?country=${event}&_page=1&_limit=10`);
         this.universities = response.data;
       } catch (error) {
         console.error(error);
